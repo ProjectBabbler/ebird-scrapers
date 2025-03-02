704128e3052e95d644371bca8d82894d8913060d
Stuart MacKay <smackay@flagstonesoftware.com>
2025-03-02 10:49:33 +0000

    test: Add faker to scrape pages from a random country

--- scripts/check.py
+++ scripts/check.py
@@ -5,14 +5,18 @@ can be scraped successfully.
 Run this script via cron, with email enabled, to get a daily, weekly,
 etc. report that format of the webpages has not changed.
 
+
 """
+
+from faker import Faker
 
 from ebird.scrapers import get_checklist, get_recent_checklists
 
-region = "US-NY-109"
+fake = Faker()
 
 
 if __name__ == "__main__":
+    region = fake.country_code()
     print("Scraping Recent Checklists page:", region)
     for checklist in get_recent_checklists(region)[:10]:
         identifier = checklist["identifier"]
